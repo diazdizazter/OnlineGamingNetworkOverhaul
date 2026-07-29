2bba0dc8117561b6789894cd00c47d37f51c3b2c
Kelley Edwin Sims <edwin.kelsi54@gmail.com>
2026-07-29 15:35:33 -0500

    Add network reset script in HOGNO_Net_Revert.ps1

---
+++ HOGNO_Net_Revert.ps1
@@ -0,0 +1,14 @@
+netsh winsock reset catalog
+Start-Sleep -Seconds 4
+netsh interface ip reset
+Start-Sleep -Seconds 4
+netsh interface tcp reset
+Start-Sleep -Seconds 4
+ipconfig /flushdns
+Start-Sleep -Seconds 4
+ipconfig /release
+Start-Sleep -Seconds 4
+ipconfig /renew
+Start-Sleep -Seconds 4
+ipconfig /all
+Start-Sleep -Seconds 4
